e6dbb3b3433f7e4803e60d11679c35879ecc809b
Kenny <kennyyylin@gmail.com>
2024-10-21 17:06:11 -0400

    Working

---
+++ clientdir/ufmymusic.h
@@ -0,0 +1,23 @@
+#ifndef UFMYMUSIC_H
+#define UFMYMUSIC_H
+
+#define MAX_FILENAME_LEN 256
+#define MAX_FILES 100
+
+#define MD5_DIGEST_LENGTH 16
+
+typedef struct {
+    int type;  
+} Message;
+
+typedef struct {
+    char filename[MAX_FILENAME_LEN];
+    unsigned char md5_hash[MD5_DIGEST_LENGTH];
+} FileEntry;
+
+typedef struct {
+    FileEntry files[MAX_FILES]; // List of filenames (max 100 files)
+    int file_count;  // Number of files
+} FileList;
+
+#endif
